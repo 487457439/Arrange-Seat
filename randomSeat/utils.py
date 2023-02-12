64a0a7cfde12e107b7e88cc3792d1314fafcf7f1
Harry <Harry0259@163.com>
2023-02-12 17:45:39 +0800

    change file struct

---
+++ randomSeat/utils.py
@@ -0,0 +1,16 @@
+class SeatArragement:
+    def __init__(self) -> None:
+        pass
+
+class Seat:
+    def __init__(self) -> None:
+        pass
+
+class Aisle:
+    def __init__(self) -> None:
+        pass
+
+class Person:
+    def __init__(self, name: int, gender: str) -> None:
+        self.name: int = name
+        self.gender:str = gender
