d49a02430716a60ff2f3e736a694196e3daebac7
Harry <Harry0259@163.com>
2023-02-15 21:48:29 +0800

    Update utils

--- randomSeat/utils.py
+++ randomSeat/utils.py
@@ -1,16 +1,30 @@
 from .dataStruct import *
 
 class SeatArragement:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,  seat_format: str) -> None:
+        seat_chart = []
+        for i in seat_format.split('\n'):
+            seat_chart.append([Aisle() if j==' ' else Seat() for j in i])
+        self.seat_chart: list[list[Seat|Aisle]] = seat_chart
+
+    def __repr__(self) -> str:
+        return f''
+            
 
 class Seat:
     def __init__(self) -> None:
         pass
 
+    def __repr__(self) -> str:
+        return f'<Seat()>'
+
+
 class Aisle:
     def __init__(self) -> None:
         pass
+
+    def __repr__(self) -> str:
+        return f'<Aisle()>'
 
 
 
