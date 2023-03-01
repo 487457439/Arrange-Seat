0bba8f941720288e416acf299a0ae6707fc64960
Gan <487457439@qq.com>
2023-03-01 20:19:44 +0800

    3.1

--- randomSeat/utils.py
+++ randomSeat/utils.py
@@ -2,10 +2,12 @@ from typing import Optional, Generator
 
 from .dataStruct import *
 
+from random import shuffle
+
 
 class Seat:
     def __init__(self) -> None:
-        self.seater: Optional[Person] = Person(name = 'None') # TODO
+        self.seater: Optional[Person] = Person(name='None')  # TODO
 
     def __repr__(self) -> str:
         return f'<Seat(seater={self.seater})>'
@@ -38,12 +40,16 @@ class SeatingChart:
         for i in seat_format.split('\n'):
             seat_chart.append([Aisle() if j == ' ' else Seat() for j in i])
         self.chart: list[list[Seat | Aisle]] = seat_chart
+        self.seats: list[list[Seat]] = self.removeAisle()
 
         # 初始化座位图的行列长度
         self.row_len = len(self.chart[0])
         self.line_len = len(self.chart)
         # ToDo:检测座位图是否合法(必须是矩形)
         pass
+
+        self.stu_list = []
+        self._loadStu(config)
 
     def __repr__(self) -> str:
         return f'<SeatingChart(...)>'
@@ -52,7 +58,10 @@ class SeatingChart:
         lines = []
         for row in self.chart:
             lines.append(
-                ''.join(self._nameFormat(i.seater.name) if isinstance(i,Seat) else ' ' for i in row)
+                ''.join(
+                    self._nameFormat(i.seater.name) if isinstance(i, Seat) else ' '
+                    for i in row
+                )
             )
             lines.append('\n')
         return ''.join(lines)
@@ -70,11 +79,30 @@ class SeatingChart:
         for i in range(len(self.chart[0])):
             yield [j[i] for j in self.chart]
 
-    def _nameFormat(self,name:str) -> str:
+    def _nameFormat(self, name: str) -> str:
         if len(name) <= 2:
             return f'[ {name} ]'
         else:
             return f'[{name[0:3]}]'
+
+    def _loadStu(self, config) -> None:
+        for people in config['people']:
+            self.stu_list.append(Person(**people))
+
+    def randomize(self) -> None:
+        shuffle(self.stu_list)
+        for line in self.seats:
+            for seat in line:
+                seat.seater = self.stu_list.pop(0)
+
+    def removeAisle(self) -> list[list[Seat]]:
+        seats = []
+        for line in self.chart:
+            for seat in line:
+                if isinstance(seat, Aisle):
+                    line.remove(seat)
+            seats.append(line)
+        return seats
 
 
     @property
